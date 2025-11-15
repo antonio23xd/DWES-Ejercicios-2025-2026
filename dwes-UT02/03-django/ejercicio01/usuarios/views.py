ddab79e46ed4d98d0a502cd5a066789cdb00f10f
Anthony Ibarra <amibarravx@fpvirtualaragon.es>
2025-11-15 22:56:18 +0100

    UT02 Django: creado modelos Usuario y Pago. Dato introducido en ambos modeos. Muestro el usuario sin pagos en el view

--- dwes-UT02/03-django/ejercicio01/usuarios/views.py
+++ dwes-UT02/03-django/ejercicio01/usuarios/views.py
@@ -1,26 +1,45 @@
 from django.shortcuts import render
+from usuarios.models import Usuario
 
 # Create your views here.
 from django.http import HttpResponse
 
 def usuario_view(request):
+
+    # Obtengo los datos del modelo Usuario
+    usuarios = Usuario.objects.all()
+    """
     datos = {
         "nombre": "Ana",
         "apellidos": "Martínez Pérez",
         "edad": 30,
         "email": "ana@example.com",
     }
-    html = f"""
-    <html>
-        <head><title>Datos de un Usuario</title></head>
-        <body>
-            <h1>Información personal</h1>
-            <!--Para incluir datos que se encuentran en la vista, usamos llaves como se ve a continuación -->
-            <p><strong>Mi Nombre:</strong> {datos['nombre']}</p>
-            <p><strong>Apellidos:</strong> {datos['apellidos']}</p>
-            <p><strong>Edad:</strong> {datos['edad']}</p>
-            <p><strong>Email:</strong> {datos['email']}</p>
-        </body>
-    </html>
     """
-    return HttpResponse(html)
+
+    html1 = f"""
+            <html>
+            <head><title>Datos de un Usuario</title></head>
+            <body>
+                <h1>Información personal</h1>"""
+    
+    # Recorro los usuarios
+    for usuario in usuarios:       
+        html2 = f"""
+                <!--Para incluir datos que se encuentran en la vista, usamos llaves como se ve a continuación -->
+                <ul>
+                    <li>
+                        <strong>Nombre: </strong>{usuario.nombre}</br>
+                        <strong>Apellidos: </strong>{usuario.apellidos}</br>
+                        <strong>DNI: </strong>{usuario.dni}</br>
+                        <strong>Email: </strong>{usuario.email}</br>
+                        <strong>Teléfono: </strong>{usuario.telefono}</br>
+                    </li>
+                </ul>                
+        """
+    html3 = """
+            </body>
+            </html>"""
+    htmlfinal = html1 + html2 + html3
+
+    return HttpResponse(htmlfinal)
